38d06e9ea2c0944af732e4d909801263c7e6bfa6
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2024-06-05 13:44:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import Navbar from "./components/layout/Navbar";
+import About from "./components/layout/About";
+import Skills from "./components/layout/Skills";
+import Projects from "./components/layout/Projects";
+import Contact from "./components/layout/Contact";
+import Footer from "./components/layout/Footer";
+import Hero from "./components/layout/Hero";
+
+export default function App() {
+	return (
+		<>
+			<Navbar />
+			<Hero />
+			<About />
+			<Skills />
+			{/* <Projects /> */}
+			{/* <Contact /> */}
+			{/* <Footer /> */}
+		</>
+	);
+}
