a9d0b1c77af1973c1b976d554ad8ad2f079c8aff
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2024-06-20 14:04:07 +0100

    feat: add footer

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ export default function App() {
 			<Skills />
 			<Projects />
 			<Contact />
-			{/* <Footer /> */}
+			<Footer />
 		</>
 	);
 }
--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,10 +1,12 @@
-// Footer.js
-export default function Footer() {
+
+function Footer() {
 	return (
 		<section className="p-10 bg-emerald-200 flex justify-center">
 			<p className="font-bold">
-				©Copyright 2023. All rights are reserved by Geeks for Geeks
+				©Copyright 2023. All rights are reserved by TaiwoSaidat
 			</p>
 		</section>
 	);
 }
+
+export default Footer;
