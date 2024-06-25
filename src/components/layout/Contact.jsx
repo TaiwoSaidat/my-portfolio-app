91e86145faf2912324c7259510bb1381d5d8e476
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2024-06-25 14:13:32 +0100

    changes to contact page

--- src/components/layout/Contact.jsx
+++ src/components/layout/Contact.jsx
@@ -1,16 +1,18 @@
 import React from "react";
+import github from '../../assets/github.svg'
+import linkedin from '../../assets/linkedin.svg'
 
 function Contact() {
 	return (
-		<section id="contact"
-				className="my-40 align-center max-w-5xl mx-auto p-3">
-				{/* heading text */}
+		<section id="contact" className="my-40 align-center max-w-5xl mx-auto p-3">
+				
+			{/* heading text */}
 			<h2 className="text-5xl font-bold text-emerald-500 text-center">Contact</h2>
 			{/* contacts */}
 			<div className="flex gap-5 justify-center my-10">
 
 				{/* github */}
-				<a rel="noreferrer"
+				{/* <a rel="noreferrer"
 					target="_blank"
 					className="text-center hover:underline"
 					href="https://github.com/TaiwoSaidat">
@@ -18,19 +20,37 @@ function Contact() {
 						<span className="font-bold">
 							 TaiwoSaidat
 						</span>
-				</a>
+				</a> */}
 
-				{/* email */}
-				<a rel="noreferrer"
-				target="_blank"
-				className="text-center hover:underline"
-				href="https://www.linkedin.com/in/taiwo-oyewale-5554791a6/">
-					LinkedIn:
-					<span className="font-bold">
-						Taiwo Oyewale
-					</span>
-				</a>
+				<div>
+					<a rel="noreferrer"
+						target="_blank"
+						className=" "
+						href="https://github.com/TaiwoSaidat">
+						<img src={github} alt="github link"  />
+					</a>
+				</div>
+<br />
+				{/* linkedin */}
+				{/* <a rel="noreferrer"
+					target="_blank"
+					className="text-center hover:underline"
+					href="https://www.linkedin.com/in/taiwo-oyewale-5554791a6/">
+						LinkedIn:
+						<span className="font-bold">
+							Taiwo Oyewale
+						</span>
+				</a> */}
+				<div>
+					<a rel="noreferrer"
+						target="_blank"
+						className=" "
+						href="https://www.linkedin.com/in/taiwo-oyewale-5554791a6/">
+						<img src={linkedin} alt="linkedin link"  />
+					</a>
+				</div>
 			</div>
+
 		</section>
 	);
 }
