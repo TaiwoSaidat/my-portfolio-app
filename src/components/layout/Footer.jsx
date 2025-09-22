1694e379de76c2698d444fbbf24cdf2048e700bb
TaiwoSaidat <oyewaletaiwo1803@gmail.com>
2025-09-22 19:01:06 +0100

    chore: footer copyright

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -2,7 +2,8 @@ function Footer() {
   return (
     <section className="p-4 bg-secondary flex justify-center">
       <p className="font-bold text-primary">
-        ©copyright 2023. All rights are reserved by <span></span>
+        {/* ©copyright */}
+        &copy;  2023. All rights are reserved by <span></span>
         <a
           href="https://github.com/TaiwoSaidat"
           className="text-primary hover:text-secondary hover:bg-primary shadow transition hover:shadow-xl hover:scale-[102%] hover:p-2 hover:rounded-md "
